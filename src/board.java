04d198221e470b65be0ca8c14a1099f297fcbcb6
sascha-greiner-adam <sascha-greiner-adam@web.de>
2013-04-22 16:05:34 +0200

    Board-Klasse fertig

--- src/board.java
+++ src/board.java
@@ -1,30 +1,49 @@
 
+
 public class board {
-	char[][]field={{'R','N','B','Q','K'},{'P','P','P','P','P'},{'.','.','.','.','.'},{'.','.','.','.','.'},{'p','p','p','p','p'},{'k','q','b','n','r'}};
+	char[][]field={{' ',' ',' ',' ',' ',' '},{' ','R','N','B','Q','K'},{' ','P','P','P','P','P'},{' ','.','.','.','.','.'},{' ','.','.','.','.','.'},{' ','p','p','p','p','p'},{' ','k','q','b','n','r'}};
 	int moveNum=1;
 	char onMove='B';
+	
 	public board() {
 	}
 	
+	public board(String pattern) {
+		String[] splitResult = pattern.split(" ");
+		this.moveNum=Integer.parseInt(splitResult[0]);
+		this.onMove=splitResult[1].charAt(0);
+		for (int i=6; i>=1; i--) {
+			for (int j=1; j<=5; j++) {
+				field[i][j]=splitResult[2].charAt(((6-i)*5)+j-1);
+			}
+		}
+	}
+	
 	public String toString() {
-		
-		return "Bla";
-		
+		String print_out=""+moveNum+" "+onMove+" ";
+		for (int i=6; i>=1; i--) {
+			for (int j=1; j<=5; j++) {
+				print_out=print_out+field[i][j];
+			}
+		}
+		return print_out;
 	}
 
 	public void print() {
 		String print_out="";
-		for (int i=5; i>=0; i--) {
+		System.out.println(moveNum+" "+onMove);
+		for (int i=6; i>=1; i--) {
 			print_out="";
-			for (int j=0; j<5; j++) {
+			for (int j=1; j<=5; j++) {
 				print_out=print_out+field[i][j];
 			}
 			System.out.println(print_out);
 		}
 	}
 	public static void main(String[] args) {
-		
-		board myBoard=new board();
+
+		board myBoard=new board("1 B kqbnrppp.....pp.....PPPPPRNBQK");
 		myBoard.print();
+		System.out.println(myBoard);
 	}
 }
